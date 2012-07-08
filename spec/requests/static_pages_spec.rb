503ac50e79501312341178199d8c1093ba33f239
Tanveer <mtanveer@live.com>
2012-07-08 01:20:18 -0400

    add "static_page features, home, help, about pages with test driven technique"

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+    it "houls have the contyent 'Sample App' " do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      # get static_pages_index_path
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+
+    end
+  end
+  describe "Help page" do
+
+  	it "should have the content 'Help'" do 
+  		visit '/static_pages/help'
+  		page.should have_content('Help')
+  	end
+  end
+	describe "About page" do
+		
+		it"should have the content 'About Us'" do
+			visit '/static_pages/about'
+			page.should have_content('About Us')
+		end
+	end
+end
+
+
